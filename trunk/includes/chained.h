3e19fcda6ecc8701105958811115ee387ed1df70
Daniel Bradshaw <daniel+commits@the-cell.co.uk>
2006-10-07 20:11:43 +0000

    including this file should include all the necessary header files

---
+++ trunk/includes/chained.h
@@ -0,0 +1,26 @@
+/*
+** Chained in Sanity
+** Core library include
+** Try to keep this neat please!
+**/
+
+#ifndef _INCLUDES_CHAINED_H_
+#define _INCLUDES_CHAINED_H_
+
+/*
+** These files are required by pretty much every part of chained ... 
+** Hence why we have them here :P
+**/
+
+#include <stdio.h>
+/*#include <stdlib.h>*/
+#include <errno.h> /* helpful ;) */
+
+#include "config.h"
+
+#include "types.h"
+#include "externs.h"
+
+#include "main.h"
+
+#endif
