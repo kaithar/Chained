90b0282bbc7ebeff017c357a0a25be02d27485c3
Daniel Bradshaw <daniel+commits@the-cell.co.uk>
2006-10-12 01:15:35 +0000

    Some headers relating to reading configs

---
+++ trunk/includes/config_reading.h
@@ -0,0 +1,14 @@
+/**
+ * @file config_reading.h
+ * @brief This contains prototypes for reading config files
+ * Chained in Sanity
+ * Config file prototypes
+ */
+
+#ifndef _INCLUDES_CONFIG_READING_H_
+#define _INCLUDES_CONFIG_READING_H_
+
+
+extern int cis_load_config(unsigned char *filename);
+
+#endif
