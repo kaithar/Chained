dd4c485a7055df0352782e8512d9c35053f5b01b
Daniel Bradshaw <daniel+commits@the-cell.co.uk>
2010-01-12 16:05:59 +0000

    More stuff

---
+++ trunk/py_module/support/net/Port.py
@@ -0,0 +1,28 @@
+import chained.primative.net as chained
+
+class Port (object):
+	# Stuff you should override...
+	name = "Some port"
+	protocol = None
+	port = 0
+
+	# Stuff you could override...
+	ip = "0.0.0.0"
+
+	# Stuff you should leave alone.
+	connection = None
+	connections = None
+	
+	def __init__(self):
+		self.connections = []
+		self.connection = chained.Port(self.name, self.ip, self.port, self)
+
+	def accepted(self, cn):
+		cn.protocol = self.protocol()
+		self.connections.append(cn)
+		self.onAccept(cn)
+
+	def onAccept(self, cn):
+		# Override me!
+		pass
+
---
+++ trunk/py_module/support/net/__init__.py
@@ -0,0 +1,1 @@
+from chained.net.Port import Port
